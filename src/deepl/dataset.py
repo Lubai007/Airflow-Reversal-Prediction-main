81edb1960e1ca6fed07eae15b8decbf37a85fc4b
Lubai007 <792544227@qq.com>
2024-09-15 16:35:29 +0800

    ceshiwancheng

--- src/deepl/dataset.py
+++ src/deepl/dataset.py
@@ -10,8 +10,6 @@ from ..config import CALCULATE_CONFIG
 
 ENV_CONFIG = CALCULATE_CONFIG()
 
-print("hello world")
-
 class NodeDataset(Dataset):
     def __init__(self, data_path:str=None, device=None):
         """Initializes an instance of the NodeDataset class.
